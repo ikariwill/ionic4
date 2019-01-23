b5a86ceba367ea5bbb4c0e6c34e7c4ee17b899ae
Willian Home <willian@wiki4fit.com.br>
2019-01-23 20:31:17 -0200

    teste

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+
+  constructor(
+    private navCtrl: NavController
+  ){ }
+
+  goBack() {
+    this.navCtrl.back();
+  }
+
+}
